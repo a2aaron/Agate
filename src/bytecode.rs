2e4ce65889925601d8fdb2b03097c48779ef7cba
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-04-07 20:11:46 -0400

    Add variants for Instructions

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -1,4 +1,6 @@
 use values::Value;
+
+// some notes:
 // start at line 1
 // (some array of code for current fn)
 // program counter -> count up for each line
@@ -13,6 +15,11 @@ use values::Value;
 
 // closure needs to know what func it's supposted be in
 
+
+/// A stack frame consists of a function (really a reference), a counter which
+/// tracks where in the function the execution is, the variables the function 
+/// needs (this includes any closure or local variables)
+#[derive(Debug)]
 struct StackFrame<'a> {
     function: &'a Function,
     function_counter: usize,
@@ -20,6 +27,10 @@ struct StackFrame<'a> {
     locals: Vec<Value>,
     closures: Vec<ClosureValue>,
 }
+
+/// A function is a structure which knows about what arguements it has as well
+/// as the instructions to run, but NOT the state of the function itself
+#[derive(Debug)]
 struct Function {
     num_args: usize,
     frame_size: usize,
@@ -28,8 +39,44 @@ struct Function {
 
 type ClosureValue = Value;
 
+/// A Register simply holds a number
+type Register = i32;
+
+#[derive(Debug)]
 enum Instruction {
-    // @TODO: what do i put here
+    // Preform math on two registers, storing the result into a third register
+    TwoOp {op: TwoOp, left: Register, right: Register, out: Register},
+    // Store value into the specified register
+    Store(Register, i32),
+    // Move a value from a register into another
+    Move {from: Register, to: Register},
+    Function, /* @TODO figure out function symbols and other stuff */
+    Return, /* @TODO either a number or a pointer to a stack frame goes here */
+    Closure, /* @TODO i have no idea what this would look like */
+    Jump(i32), // Jump (relative)
+    // Compare two registers, if the result is true then jump
+    JumpIf {op: CompareOp, left: Register, right: Register, jump: i32},
+}
+
+#[derive(Debug)]
+enum CompareOp {
+    Equals,
+    LessThan,
+    GreaterThan,
+    LessOrEqualTo,
+    GreaterOrEqualTo,
+}
+
+#[derive(Debug)]
+enum TwoOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    AND,
+    OR,
+    XOR,
 }
 
 #[cfg(test)]
